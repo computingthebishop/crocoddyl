7dff72150cbc64da8317e9c8fa4b5dfc620d69fc
Carlos Mastalli <carlos.mastalli@ed.ac.uk>
2020-10-24 18:52:07 +0100

    [doc] Clarified that we need to run calc first (in calcDiff)

--- include/crocoddyl/core/action-base.hpp
+++ include/crocoddyl/core/action-base.hpp
@@ -63,6 +63,8 @@ namespace crocoddyl {
  * Jacobians of the inequality constraints, and
  *  - \f$\mathbf{h_x}\in\mathbb{R}^{nh\times ndx}\f$ and \f$\mathbf{h_u}\in\mathbb{R}^{nh\times nu}\f$ are the
  * Jacobians of the equality constraints.
+ * Additionally, it is important remark that `calcDiff()` computes the derivates using the latest stored values by
+ * `calc()`. Thus, we need to run first `calc()`.
  *
  * \sa `calc()`, `calcDiff()`, `createData()`
  */
